b4572480183f6aea02a51fe3443212c48a6c1d87
Felix Guevara <fguevara1@mail.valenciacollege.edu>
2026-01-25 13:11:23 -0500

    Update page.tsx
    Update logo

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,15 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
+          src="/movie-camera.svg"
+          alt="camera"
+          width={125}
+          height={125}
           priority
         />
 
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left w-full">
-          <h1 className="w-full text-3xl font-semibold leading-10 tracking-tight text-blue-600">
+          <h1 className="w-full text-3xl font-semibold leading-10 tracking-tight text-[rgb(0,74,156)]">
             Welcome to initial deployment of MyMovieShelf.
           </h1>
 
@@ -23,15 +23,6 @@ export default function Home() {
           </p>
         </div>
 
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left w-full">
-            <Image
-              className="dark:invert"
-              src="/movie-camera.svg"
-              alt="camera"
-              width={100}
-              height={100}
-            />
-        </div>
       </main>
     </div>
   );
