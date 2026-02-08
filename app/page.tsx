861f791a1544a393e112843fbb639a9ed04cc557
Felix Guevara <fguevara1@mail.valenciacollege.edu>
2026-02-08 10:23:12 -0500

    Update Landing page

--- app/page.tsx
+++ app/page.tsx
@@ -9,21 +9,21 @@ import {
 
 export default function Home() {
   return (
-    <div className="mx-auto max-w-4xl px-4 py-10">
-      <main className="flex w-full max-w-3xl flex-col items-center justify-center py-10 px-6 dark:bg-black sm:items-start">
+    <div className="mx-auto max-w-4xl py-10">
+      <main className="flex w-full max-w-3xl flex-col items-center justify-center py-10 dark:bg-black sm:items-start">
       
-        <div className="mx-auto max-w-4xl px-4 py-10">
+        <div className="mx-auto max-w-4xl py-10">
           {/* Top section */}
           <section className="mb-6">
-            <h2 className="text-4xl font-extrabold tracking-tight text-gray-900">
-              About <span className="text-[rgb(0,76,157)]">What would you like to do?</span>
-            </h2>
+            <h3 className="text-4xl font-extrabold tracking-tight text-gray-900">
+              <span className="text-[rgb(0,76,157)]">What would you like to do?</span>
+            </h3>
           </section>
 
           
           {/* Action grid */}
           <section aria-label="Quick actions">
-            <div className="grid grid-cols-2 gap-3 sm:grid-cols-4 lg:grid-cols-3">
+            <div className="grid grid-cols-4 gap-3 sm:grid-cols-2 lg:grid-cols-1">
               <ActionCard href="/movies" label="My Movie Library" icon={<Film className="h-5 w-5" />} />
               <ActionCard href="/stats" label="My Movie Shelfs" icon={<BarChart3 className="h-5 w-5" />} />
               <ActionCard href="/about" label="About Us" icon={<BookOpenText className="h-5 w-5" />} />
