e73f6bc594803cd7553ac53841fd1ffba48d90f6
Felix Guevara <fguevara1@mail.valenciacollege.edu>
2026-02-14 18:08:31 -0500

    Update page.tsx

--- app/movieLibrary/[id]/page.tsx
+++ app/movieLibrary/[id]/page.tsx
@@ -2,11 +2,6 @@ import MovieDetailsClient from "./MovieDetailsClient";
 import { notFound } from "next/navigation";
 
 export default function MovieDetailsPage({ params }: { params: { id?: string } }) {
-  console.log("Route params:", params); // <-- should show { id: "1" } (or whatever)
-  if (!params?.id) {
-    // If id is missing, send to 404 (prevents empty id in the UI)
-    notFound();
-  }
-  // decode in case the id is URL encoded
+  if (!params?.id) notFound();
   return <MovieDetailsClient id={decodeURIComponent(params.id)} />;
 }
