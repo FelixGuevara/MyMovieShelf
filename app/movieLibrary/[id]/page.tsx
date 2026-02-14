a6795d7237a331a58ac37ac8be05f8d8178e4624
Felix Guevara <fguevara1@mail.valenciacollege.edu>
2026-02-14 18:19:11 -0500

    Update page.tsx

--- app/movieLibrary/[id]/page.tsx
+++ app/movieLibrary/[id]/page.tsx
@@ -2,6 +2,6 @@ import MovieDetailsClient from "./MovieDetailsClient";
 import { notFound } from "next/navigation";
 
 export default function MovieDetailsPage({ params }: { params: { id?: string } }) {
-  if (!params?.id) notFound();
-  return <MovieDetailsClient id={decodeURIComponent(params.id)} />;
+
+  return <MovieDetailsClient id={"1"} />;
 }
